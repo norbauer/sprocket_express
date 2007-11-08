f91045dafdfab9baa7d8fa9e55e8c03fa0e61d83
Ryan Norbauer <ryan@norbauer.com>
2007-11-08 07:36:13 +0000

    more generator fixes

--- generators/sprocket_fulfillment_migrations_generator.rb
+++
@@ -1,9 +0,0 @@
-class SprocketFulfillmentMigrationsGenerator < Rails::Generator::Base
-
-  def manifest
-    record do |m|
-      m.migration_template 'migration.rb', 'db/migrate'
-    end
-  end
-
-end
