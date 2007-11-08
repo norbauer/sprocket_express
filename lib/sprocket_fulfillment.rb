a7ac111ac4042df9b6f0952060ad71fcd77d9de9
Ryan Norbauer <ryan@norbauer.com>
2007-11-08 06:40:42 +0000

    fixed type in include

--- lib/sprocket_fulfillment.rb
+++ lib/sprocket_fulfillment.rb
@@ -1,6 +1,6 @@
 # Include the two models defined in the library.
 module SprocketFulfillment
   require 'sprocket_fulfillment_order'
-  require 'sprocket_fulfillment_order_line_items'
+  require 'sprocket_fulfillment_order_line_item'
   include 'map'
 end
