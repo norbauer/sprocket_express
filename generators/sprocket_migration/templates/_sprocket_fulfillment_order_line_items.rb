a189601bb99845c6ad8a4730a7292a140d6a5383
Ryan Norbauer <ryan@norbauer.com>
2007-11-25 14:33:23 +0000

    desisted in my futile attempt to DRY up the generator code

--- generators/sprocket_migration/templates/_sprocket_fulfillment_order_line_items.rb
+++
@@ -1,7 +0,0 @@
-create_table :sprocket_fulfillment_order_line_items, :force => true do |t|
-  t.integer :sprocket_fulfillment_order_id
-  t.string :sku, :limit => 20
-  t.integer :quantity
-  t.decimal :price, :precision => 6, :scale => 2
-  t.integer :discount
-end
--- spec/db/schema.rb
+++ spec/db/schema.rb
@@ -1,5 +1,45 @@
 ActiveRecord::Schema.define(:version => 0) do
-  # To keep things DRY wth migration.rb
-  eval File.open(File.dirname(__FILE__) + '/../../generators/sprocket_migration/templates/_sprocket_fulfillment_orders.rb').read
-  eval File.open(File.dirname(__FILE__) + '/../../generators/sprocket_migration/templates/_sprocket_fulfillment_order_line_items.rb').read
+  create_table :sprocket_fulfillment_orders, :force => true do |t|
+    t.string :ship_via, :limit => 3
+    t.string :billing_last_name, :limit => 20
+    t.string :billing_first_name, :limit => 15
+    t.string :billing_company, :limit => 40
+    t.string :billing_address_1, :limit => 40
+    t.string :billing_address_2, :limit => 40
+    t.string :billing_city, :limit => 20
+    t.string :billing_state, :limit => 3
+    t.string :billing_zipcode, :limit => 10
+    t.string :billing_phone, :limit => 14
+    t.string :billing_email, :limit => 50
+    t.string :billing_country, :limit => 3
+    t.string :shipping_last_name, :limit => 20
+    t.string :shipping_first_name, :limit => 15
+    t.string :shipping_company, :limit => 40
+    t.string :shipping_address_1, :limit => 40
+    t.string :shipping_address_2, :limit => 40
+    t.string :shipping_city, :limit => 20
+    t.string :shipping_state, :limit => 3
+    t.string :shipping_zipcode, :limit => 10
+    t.string :shipping_phone, :limit => 14
+    t.string :shipping_email, :limit => 50
+    t.string :shipping_country, :limit => 3
+    t.boolean :shipping_same_as_billing, :default => true
+    t.datetime :date_of_original_purchase_transaction
+    t.integer :id_from_original_purchase_transaction
+    t.datetime :hold_date
+    t.decimal :shipping_fee, :precision => 8, :scale => 2
+    t.string :gift_message_1, :limit => 70
+    t.string :gift_message_2, :limit => 70
+    t.string :gift_message_3, :limit => 70
+    t.boolean :sent_for_fulfillment, :default => false
+    t.timestamps
+  end
+
+  create_table :sprocket_fulfillment_order_line_items, :force => true do |t|
+    t.integer :sprocket_fulfillment_order_id
+    t.string :sku, :limit => 20
+    t.integer :quantity
+    t.decimal :price, :precision => 6, :scale => 2
+    t.integer :discount
+  end
 end
