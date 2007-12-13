d915b8be6187c923f3a497ebbccc27603757676f
Ryan Norbauer <ryan@norbauer.com>
2007-12-13 04:23:40 +0000

    fixed shipping_same_as_billing_bug

--- lib/sprocket_express_order.rb
+++ lib/sprocket_express_order.rb
@@ -3,16 +3,17 @@ class SprocketExpressOrder < ActiveRecord::Base
   include SprocketExpress
         
   has_many :sprocket_express_order_line_items
-  
-  attr_accessor :shipping_same_as_billing
-  
+    
   validates_presence_of :billing_first_name, :billing_last_name, :billing_address_1, :billing_city, :billing_country, :billing_zipcode, :ship_via
   validates_presence_of :billing_state, :if => Proc.new { |order| !order.foreign? }
   validates_length_of :billing_city, :shipping_city, :maximum => 20, :allow_nil => true
   validates_length_of :billing_address_1, :billing_address_2, :shipping_address_1, :shipping_address_2, :maximum => 40, :allow_nil => true
+  
+  validates_presence_of :shipping_zipcode, :if => Proc.new { |order| !order.shipping_same_as_billing? }
+  validates_presence_of :shipping_address_1, :if => Proc.new { |order| !order.shipping_same_as_billing? }
 
   validates_format_of :billing_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  validates_format_of :shipping_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :if => Proc.new { |order| !order.shipping_same_as_billing? }
+  validates_format_of :shipping_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :if => Proc.new { |order| !order.shipping_same_as_billing? && !order.shipping_email.blank? }
   
   validates_inclusion_of :billing_country,  :in => SprocketExpress::country_codes, :message => "is not valid. Please check country mapping for valid country codes."
   validates_inclusion_of :shipping_country, :in => SprocketExpress::country_codes, :if  => Proc.new { |order| !order.shipping_same_as_billing? }, :message => "is not valid. Please check country mapping for valid country codes.", :allow_nil => true
@@ -27,10 +28,10 @@ class SprocketExpressOrder < ActiveRecord::Base
   before_validation :remove_state_if_foreign
     
   def foreign?
-    if shipping_same_as_billing?
-      !billing_country.blank? && (billing_country != SprocketExpress::country_names_to_country_codes['United States'])
+    if self.shipping_same_as_billing?
+      !self.billing_country.blank? && (self.billing_country != SprocketExpress::countries('United States'))
     else
-      !shipping_country.blank? && (shipping_country != SprocketExpress::country_names_to_country_codes['United States'])
+      !self.shipping_country.blank? && (self.shipping_country != SprocketExpress::countries('United States'))
     end
   end
 
@@ -48,15 +49,15 @@ class SprocketExpressOrder < ActiveRecord::Base
   end
     
   def clear_out_shipping_values_if_shipping_same_as_billing
-    if shipping_same_as_billing?
+    if self.shipping_same_as_billing?
       self.attribute_names.grep(/^shipping_/).each do |attribute|
-        self.send("#{attribute}=",nil)
+        self.send("#{attribute}=",nil) unless attribute == 'shipping_same_as_billing'
       end
     end
   end
   
   def remove_state_if_foreign
-    if foreign? 
+    if foreign?
       self.billing_state = nil
       self.shipping_state = nil
     end
@@ -65,12 +66,12 @@ class SprocketExpressOrder < ActiveRecord::Base
   def check_state_values
     return true if foreign?
     
-    if billing_state.size != 2
+    if self.billing_state.size != 2
       errors.add('billing_state', "is not valid. If shipping in US, please use 2 character state code.")
     end
     
-    if !shipping_same_as_billing? && !shipping_state.blank?
-      if shipping_state.size != 2 || shipping_state.size != 2
+    if !self.shipping_same_as_billing? && !self.shipping_state.blank?
+      if self.shipping_state.size != 2 || self.shipping_state.size != 2
         errors.add('shipping_state', "is not valid. If shipping in US, please use 2 character state code.")        
       end
     end
